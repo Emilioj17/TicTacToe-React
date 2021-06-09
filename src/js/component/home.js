9e3e9a4914cb4512abf0426c359a50f8f50493ce
Emilio Jelves <ejjelves@gmail.com>
2021-06-09 16:17:28 -0400

    Version en Transicion

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,119 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+// const Clickeado = () => {
+// 	if (squareClick[1] == undefined) {
+// 		return <div></div>;
+// 	} else if (squareClick[1] != undefined) {
+// 		return <div>1</div>;
+// 	}
+// };
 
-//create your first component
+let squareClick = [];
+
+const Clickeado = evento => {
+	if (squareClick[1] == undefined) {
+		return <div></div>;
+	} else if (squareClick[1] != undefined) {
+		console.log(`Hola desde ${evento} ${squareClick[1]}`);
+		Home();
+		return <div>2</div>;
+	}
+};
+
+const Click = (casilla, evento) => {
+	let aux;
+	for (let x in squareClick) {
+		// aux = squareClick[x] == casilla ? (aux = "") : (aux = casilla);
+		if (squareClick[x] == casilla) {
+			aux = "";
+			break;
+		} else {
+			aux = casilla;
+		}
+	}
+	// aux == "" ? console.log("nada") : (squareClick.push(aux));
+	if (aux != "") {
+		squareClick.push(aux);
+		Clickeado(evento);
+	}
+};
+
 export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center bg-secondary">
+			<div className="superior text-white">
+				<h1>
+					<div className="display-4 d-inline">Tic Tac Toe</div> in
+					React.js
+				</h1>
+			</div>
+			<div className="central d-flex justify-content-center py-3">
+				<div className="game">
+					<div
+						onClick={e => Click(1, e.target.className)}
+						className="topLeft">
+						<div>
+							<Clickeado />
+						</div>
+					</div>
+					<div
+						onClick={e => Click(2, e.target.className)}
+						className="topCenter">
+						<div>
+							<Clickeado />
+						</div>
+					</div>
+					<div
+						onClick={e => Click(3, e.target.className)}
+						className="topRight">
+						<div>
+							<Clickeado />
+						</div>
+					</div>
+					<div
+						onClick={e => Click(4, e.target.className)}
+						className="middleLeft">
+						<div>
+							<Clickeado />
+						</div>
+					</div>
+					<div
+						onClick={e => Click(5, e.target.className)}
+						className="middleCenter">
+						<div>
+							<Clickeado />
+						</div>
+					</div>
+					<div
+						onClick={e => Click(6, e.target.className)}
+						className="middleRight">
+						<div>
+							<Clickeado />
+						</div>
+					</div>
+					<div
+						onClick={e => Click(7, e.target.className)}
+						className="bottomLeft">
+						<div>
+							<Clickeado />
+						</div>
+					</div>
+					<div
+						onClick={e => Click(8, e.target.className)}
+						className="bottomCenter">
+						<div>
+							<Clickeado />
+						</div>
+					</div>
+					<div
+						onClick={e => Click(9, e.target.className)}
+						className="bottomRight">
+						<div>
+							<Clickeado />
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
